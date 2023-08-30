6f928825744e77f90d7310a9e1e7920d9cd89ba7
Aarthy Adhibagavan <aarthyvasubhav003@gmail.com>
2023-08-30 05:09:39 -0700

    react arrow function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,31 +2,15 @@ import React from "react";
 import Card from "./Card";
 import emojies from "../emojies"
 
-function loopCard(emojie){
-return <Card 
-key = {emojie.id}
-emoji = {emojie.emoji}
-name = {emojie.name}
-desc = {emojie.meaning}
-/>
-}
-
-const getMean = emojies.map(function (emojie){
-    return <p>{emojie.meaning.substring(0,100)}</p>
-});
-
-
-console.log(getMean);
-
-
-
 function App(){
     return <div>
     <h1>EMOJIES Meaning</h1>
     <dl className="dictionary">
-    {emojies.map(loopCard)}
+    {emojies.map(emojie => (
+        <Card key = {emojie.id} emoji = {emojie.emoji} name = {emojie.name} desc = {emojie.meaning}/>
+    ))}
     </dl>
-    <p>{getMean}</p>
+    <p>{emojies.map(emojie =>  <p>{emojie.meaning.substring(0,100)}</p>)}</p>
     </div>
 }
 
